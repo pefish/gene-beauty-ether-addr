44ea907d598869cf5d38de7c93f058707a5d684e
pefish <pefish@qq.com>
2021-03-11 11:43:39 +0800

    add

---
+++ cmd/gene-beauty-ether-addr/command/inspect.go
@@ -0,0 +1,49 @@
+package command
+
+import (
+	"flag"
+	"github.com/pefish/go-coin-eth"
+	"github.com/pefish/go-commander"
+	go_config "github.com/pefish/go-config"
+	go_logger "github.com/pefish/go-logger"
+)
+
+type InspectCommand struct {
+
+}
+
+func NewInspectCommand() *InspectCommand {
+	return &InspectCommand{
+
+	}
+}
+
+func (dc *InspectCommand) DecorateFlagSet(flagSet *flag.FlagSet) error {
+	flagSet.String("path", "m/0/0", "path")
+	flagSet.String("mnemonic", "mnemonic", "mnemonic")
+	flagSet.String("pass", "pefish", "password")
+	return nil
+}
+
+
+func (dc *InspectCommand) OnExited(data *commander.StartData) error {
+	return nil
+}
+
+func (dc *InspectCommand) Start(data *commander.StartData) error {
+	path := go_config.Config.MustGetString("path")
+	mnemonic := go_config.Config.MustGetString("mnemonic")
+	pass := go_config.Config.MustGetString("pass")
+
+	wallet := go_coin_eth.NewWallet()
+	seed := wallet.SeedHexByMnemonic(mnemonic, pass)
+	result, err := wallet.DeriveFromPath(seed, path)
+	if err != nil {
+		return err
+	}
+	go_logger.Logger.InfoF("Address: %s", result.Address)
+	go_logger.Logger.InfoF("Private key: %s", result.PrivateKey)
+
+	return nil
+}
+
--- cmd/gene-beauty-ether-addr/main.go
+++ cmd/gene-beauty-ether-addr/main.go
@@ -9,8 +9,7 @@ import (
 
 func main() {
 	commanderInstance := commander.NewCommander(version.AppName, version.Version, version.AppName + " 是一个生成漂亮的以太坊地址的工具，祝你玩得开心。作者：pefish")
-	//commanderInstance.RegisterSubcommand("client", client.NewClient())
-	//commanderInstance.RegisterSubcommand("server", server.NewServer())
+	commanderInstance.RegisterSubcommand("inspect", command.NewInspectCommand())
 	commanderInstance.RegisterDefaultSubcommand(command.NewDefaultCommand())
 	err := commanderInstance.Run()
 	if err != nil {
